2314e43b0ef884c55d4c8f153ec227a248285687
Ioan Indreias <indreias@gmail.com>
2021-01-03 16:41:16 +0200

    Update test002.py

--- test002.py
+++ test002.py
@@ -3,7 +3,3 @@
 def my_sum(first, second):
     """This function will return the sum of two values"""
     return first + second
-
-def test_my_sum():
-    """This is the test of my_sum function"""
-    assert my_sum(-1, 1) == 0
